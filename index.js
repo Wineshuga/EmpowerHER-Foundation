204aedf6f05ba55249cb75db59eb6473aa2116d4
wineshuga <uzochukwuwinnie@gmail.com>
2023-06-28 11:58:47 +0100

    Make more-btn show more featured speakers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.menu-icon');
 const menu = document.querySelector('.mobile-menu');
 const closeIcon = document.querySelector('.fa-close');
+const moreBtn = document.querySelector('.more-btn');
+const moreFeature = document.querySelectorAll('.featured-container > section:nth-child(n+3)');
 
 hamburger.addEventListener('click', () => {
   menu.style.display = 'block';
@@ -8,4 +10,14 @@ hamburger.addEventListener('click', () => {
 
 closeIcon.addEventListener('click', () => {
   menu.style.display = 'none';
+});
+
+moreBtn.addEventListener('click', () => {
+  moreFeature.forEach((item) => {
+    if (item.style.display === 'none') {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 });
